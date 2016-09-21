f1df83276e9e18a52deed8e36ff711eef1c691dc
Jeremy Wells <jemmyw@gmail.com>
2016-09-21 22:20:09 +1200

    feat(Bot): Add more than just finish deploy message for deployments

--- src/bot/release.js
+++ src/bot/release.js
@@ -3,58 +3,76 @@ import rm from 'remove-markdown'
 import {trim, T} from 'ramda'
 
 export default function Release({server, slack}) {
+  function readChangelogRelease(path) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, 'utf8', (err, data) => {
+        if (err) { return reject(err) }
+        fs.unlink(path, T)
+        const releases = data.toString().split(/<a name.*<\/a>/).slice(1)
+        const release = rm(trim(releases[0]))
+
+        resolve(release)
+      })
+    })
+  }
+
+  const lifecycles = {
+    start: ({name, version, stage}) => `Started deploying ${name} ${version} to ${stage}`,
+    finish: ({name, version, stage}) => `${name} version ${version} released to ${stage}`
+  }
+
+  function postMessage(details) {
+    const {version, stage, branch, lifecycle, release} = details
+    const message = lifecycles[lifecycle || 'finish'](details)
+
+    const detailsAttachment = {
+        fields: [
+          {
+            title: 'Stage',
+            value: stage,
+            short: true,
+          },
+          {
+            title: 'Version',
+            value: version,
+            short: true,
+          },
+          {
+            title: 'Branch',
+            value: branch,
+            short: true,
+          },
+        ],
+      }
+
+    const attachments = [detailsAttachment]
+
+    if (release) {
+      const changelogUrl = `https://github.com/SparksNetwork/sparks-backend/blob/v${version}/CHANGELOG.md`
+
+      attachments.push({
+        title: 'Changelog',
+          title_link: changelogUrl,
+        text: release,
+      })
+    }
+
+    slack.chat.postMessage('#z-dev', message, {as_user: true, attachments})
+  }
+
   /**
   * CircleCI will post to this route when a release happens
   *
   * TODO: Make this generic
   */
   server.post('/release', function(req, res, next) {
-    const {stage, name, branch, version} = req.body
     const changelog = req.files.changelog
 
     if (changelog) {
-      fs.readFile(changelog.path, 'utf8', (err, data) => {
-        fs.unlink(changelog.path, T)
-
-        const releases = data.toString().split(/<a name.*<\/a>/)
-        const release = rm(trim(releases[1]))
-        const changelogUrl = `https://github.com/SparksNetwork/sparks-backend/blob/v${version}/CHANGELOG.md`
-
-        slack.chat.postMessage(
-          '#z-dev',
-          `${name} version ${version} released to ${stage}`,
-          {
-            as_user: true,
-            attachments: [
-              {
-                text: 'ok',
-                fields: [
-                  {
-                    title: 'Stage',
-                    value: stage,
-                    short: true,
-                  },
-                  {
-                    title: 'Version',
-                    value: version,
-                    short: true,
-                  },
-                  {
-                    title: 'Branch',
-                    value: branch,
-                    short: true,
-                  },
-                ],
-              },
-              {
-                title: 'Changelog',
-                title_link: changelogUrl,
-                text: release,
-              },
-            ],
-          }
-        )
-      })
+      readChangelogRelease(changelog.path)
+        .then(release => postMessage({...details, release}))
+    } else {
+      postMessage(details)
     }
 
     res.send({ok: true})
